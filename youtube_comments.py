7b5566dd3bf1a86588781169fdaa1c0959c05a60
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2022-08-07 20:17:57 +0200

    Update youtube_comments.py

--- youtube_comments.py
+++ youtube_comments.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import html
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -14,7 +15,7 @@ os.environ['WDM_LOG_LEVEL'] = '0'
 class YouTubeComments:
     _api_service_name: str = field(default="youtube", init=False)
     _api_version: str = field(default="v3", init=False)
-    developer_key: str = "<api key>"
+    developer_key: str = "AIzaSyBi7zzOcY7DDJgVQTvWhvBSRD0TzzyuVqo"
 
     youtube = build(_api_service_name.default, _api_version.default, developerKey=developer_key)
 
@@ -47,58 +48,61 @@ class YouTubeComments:
         return title, likes, views
 
     def get_comments(self, video_url: str, max_results=50) -> dict:
-        video_id = video_url.replace("https://www.youtube.com/watch?v=", "").split("&t=")[0].split("&list=")[0]
-        print(f"Getting commnents for video: {video_url}, max results: {max_results}")
+        try:
+            video_id = video_url.replace("https://www.youtube.com/watch?v=", "").split("&t=")[0].split("&list=")[0]
+            print(f"Getting commnents for video: {video_url}, max results: {max_results}")
 
-        comments_list = []
-        likes_list = []
-        published_at_list = []
+            comments_list = []
+            likes_list = []
+            published_at_list = []
 
-        next_page_token = None
-        max_res_reached = False
-        comment_number = 0
+            next_page_token = None
+            max_res_reached = False
+            comment_number = 0
 
-        while True:
-            request = self.youtube.commentThreads().list(
-                part="snippet,replies",
-                videoId=video_id,
-                maxResults=50,
-                pageToken=next_page_token
-            )
-            response = request.execute()
+            while True:
+                request = self.youtube.commentThreads().list(
+                    part="snippet,replies",
+                    videoId=video_id,
+                    maxResults=50,
+                    pageToken=next_page_token
+                )
+                response = request.execute()
 
-            for item in response['items']:
-                if comment_number < max_results:
-                    comment = html.unescape(item['snippet']['topLevelComment']['snippet']['textDisplay'].replace("<br>", ""))
-                    likes = item['snippet']['topLevelComment']['snippet']['likeCount']
-                    published_at = item['snippet']['topLevelComment']['snippet']['publishedAt']
+                for item in response['items']:
+                    if comment_number < max_results:
+                        comment = html.unescape(item['snippet']['topLevelComment']['snippet']['textDisplay'].replace("<br>", ""))
+                        likes = item['snippet']['topLevelComment']['snippet']['likeCount']
+                        published_at = item['snippet']['topLevelComment']['snippet']['publishedAt']
 
-                    comments_list.append(comment)
-                    likes_list.append(likes)
-                    published_at_list.append(published_at)
+                        comments_list.append(comment)
+                        likes_list.append(likes)
+                        published_at_list.append(published_at)
 
-                    comment_number += 1
-                else:
-                    max_res_reached = True
+                        comment_number += 1
+                    else:
+                        max_res_reached = True
+                        break
+
+                try:
+                    next_page_token = response['nextPageToken']
+                except KeyError:
+                    next_page_token = None
+                print(f"Comments: {comment_number}/{max_results}")
+
+                if not next_page_token or max_res_reached:
                     break
 
-            try:
-                next_page_token = response['nextPageToken']
-            except KeyError:
-                next_page_token = None
-            print(f"Comments: {comment_number}/{max_results}")
+            data = {
+                "Date": published_at_list,
+                "Likes": likes_list,
+                "Comments": comments_list,
+            }
 
-            if not next_page_token or max_res_reached:
-                break
-
-        data = {
-            "Date": published_at_list,
-            "Likes": likes_list,
-            "Comments": comments_list,
-        }
-
-        print("Done")
-        return data
+            print("Done")
+            return data
+        except HttpError:
+            return {}
 
 
 if __name__ == '__main__':
